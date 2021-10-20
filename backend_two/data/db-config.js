5db32f68c5ff9dd991e3cecf7fedd26aa555681f
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-10-20 07:18:47 -0500

    debugger working

--- backend_two/data/db-config.js
+++ backend_two/data/db-config.js
@@ -2,5 +2,4 @@ const configs = require("../knexfile");
 const knex = require("knex");
 
 const enviroment = process.env.NODE_ENV || "development";
-
 module.exports = knex(configs[enviroment]);
--- backend_two/knexfile.js
+++ backend_two/knexfile.js
@@ -1,16 +1,16 @@
 module.exports = {
-    //different configs for different dbs(dev, prod, testing, qa, staging)
-    development: {
-      client: "sqlite3",
-      connection: {
-        filename: "./data/anywhere_fitness.db3",
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: "./data/migrations",
-      },
-      seeds: {
-        directory: "./data/seeds",
-      },
+  //different configs for different dbs(dev, prod, testing, qa, staging)
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: `${__dirname}/data/anywhere_fitness.db3`,
     },
-  };
+    useNullAsDefault: true,
+    migrations: {
+      directory: `${__dirname}/data/migrations`, // put in an enviroment variable
+    },
+    seeds: {
+      directory: `${__dirname}/data/seeds`,
+    },
+  },
+};
